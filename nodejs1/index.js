4ae3a7494f8de759a88321e60a9bef3e615d7f39
JovHariono <jovanhariono18@gmail.com>
2023-05-11 21:45:20 +0700

    install express.js

--- nodejs1/index.js
+++ nodejs1/index.js
@@ -1,2 +1,13 @@
-const data = 'testing'
-console.log(data)
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
