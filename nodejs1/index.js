b26de18652df53886015eeb33eabb1fbba2ab509
JovHariono <jovanhariono18@gmail.com>
2023-05-22 12:57:32 +0700

    belajar node js part 2, menambahkan nodemon dan juga menggunakan postman

--- nodejs1/index.js
+++ nodejs1/index.js
@@ -1,13 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 
-//routes
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("utama nich");
+});
+
+//route / URL / endpoint utama
+app.get("/hello", (req, res) => {
+  console.log({ urlParam: req.query });
+  const username = req.body.username
+  if(username === usernameFromDbExist){
+    res.status(400).send('username sudah digunakan')
+  }
+  res.send("Hello World!");
+});
+
+app.post("/login", (req, res) => {
+  console.log({ requestFromOutside: req.body });
+  res.send("login berhasil");
+});
+
+app.put("/username", (req, res) => {
+  console.log({ updateData: req.body });
+  res.send("update berhasil");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
